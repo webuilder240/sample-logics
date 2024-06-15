6f0cc4515aacfffc2764c43d5f91fdd333fbc87e
Takuya Nishio <littlecub240@gmail.com>
2024-06-15 09:25:53 +0900

    vitest

--- src/components/Counter/logics.test.js
+++ src/components/Counter/logics.test.js
@@ -1,5 +1,10 @@
 import { increment, decrement } from './logics';
+import { expect, test } from "vitest"
 
-test('2', () => {
+test('increment', () => {
   expect(increment(1)).toBe(2);
 });
+
+test("decrement", () => {
+  expect(decrement(1)).toBe(0)
+})
--- src/components/Counter/logics.ts
+++ src/components/Counter/logics.ts
@@ -1,7 +1,7 @@
-export function increment(count: number) {
+export function increment(count: number): number {
   return count+=1
 }
 
-export function decrement(count: number) {
+export function decrement(count: number): number {
   return count-=1
 }
